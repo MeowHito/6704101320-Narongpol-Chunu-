1e61e860bea6bce93437908a3ea93a1a22458f4c
rattyC <rattapoommaru@gmail.com>
2025-10-07 21:17:31 +0700

    Add basic student form

--- quasar.config.js
+++ quasar.config.js
@@ -77,7 +77,12 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
-      config: {},
+      config: {
+        notify: {
+          position: 'top-right',
+          timeout: 3000
+        }
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -90,7 +95,9 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
